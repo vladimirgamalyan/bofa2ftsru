50400ff5cac86bc6c99ac90647e69b8aa485fc34
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2018-06-07 16:16:34 +0300

    Add main function

---
+++ bofa2ftsru.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def convert(input_dir, output_file):
+    print(input_dir, output_file)
+
+
+if __name__ == '__main__':
+    convert(sys.argv[1], sys.argv[2])
